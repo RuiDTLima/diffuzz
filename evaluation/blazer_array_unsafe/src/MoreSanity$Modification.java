15f52c5248408eed987910b71905ac3af80b1399
RuiDTLima <rui_lima24@hotmail.com>
2020-05-11 12:09:05 +0100

    Execution of new tool in control-flow based timing side-channel vulnerabilities examples.

---
+++ evaluation/blazer_array_unsafe/src/MoreSanity$Modification.java
@@ -0,0 +1,107 @@
+class MoreSanity$Modification {
+    public static boolean array_safe(int[] a, int taint) {
+        int t;
+        if (taint < 0) {
+            int i = a.length - 1;
+            while (i >= 0) {
+                t = a[i];
+                i--;
+            } 
+        } else {
+            int i = 0;
+            while (i < a.length) {
+                t = a[i] / 2;
+                i++;
+            } 
+        }
+        return false;
+    }
+
+    public static boolean array_unsafe(int[] a, int taint) {
+        System.out.println(a.length);
+        int t;
+        if (taint < 0) {
+            int i = a.length - 1;
+            while (i >= 0) {
+                t = a[i];
+                i--;
+            } 
+        } else {
+            int i = 0;
+            t = a[i] / 2;
+            i = a.length;
+        }
+        return false;
+    }
+
+    public static boolean array_unsafe$Modification(int[] a, int taint) {
+        System.out.println(a.length);
+        int t;
+        if (taint < 0) {
+            int i = a.length - 1;
+            while (i >= 0) {
+                t = a[i];
+                i--;
+            } 
+            int $3 = 0;
+            $3 = a[i] / 2;
+            $3 = a.length;
+        } else {
+            int i = 0;
+            t = a[i] / 2;
+            i = a.length;
+            int $2 = a.length - 1;
+            while ($2 >= 0) {
+                $2 = a[i];
+                $2--;
+            } 
+        }
+        return false;
+    }
+
+    public static boolean loopAndbranch_safe(int a, int taint) {
+        int i = a;
+        if (taint < 0) {
+            while (i > 0) {
+                i--;
+            } 
+        } else {
+            taint = taint + 10;
+            if (taint >= 10) {
+                int j = a;
+                while (j > 0) {
+                    j--;
+                } 
+            } else if (a < 0) {
+                int k = a;
+                while (k > 0)
+                    k--;
+
+            }
+        }
+        return true;
+    }
+
+    public static boolean loopAndbranch_unsafe(int a, int taint) {
+        int i = a;
+        if (taint < 0) {
+            while (i > 0) {
+                i--;
+            } 
+        } else {
+            taint = taint - 10;
+            if (taint >= 10) {
+                int j = a;
+                while (j > 0) {
+                    j--;
+                } 
+            } else if (a < 0) {
+                int k = a;
+                while (k > 0)
+                    k--;
+
+            }
+        }
+        return true;
+    }
+}
