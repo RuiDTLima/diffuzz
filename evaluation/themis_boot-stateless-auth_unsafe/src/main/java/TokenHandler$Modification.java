0911a3e3a7cb0269c86ec5dde03dec86e615855f
RuiDTLima <rui_lima24@hotmail.com>
2020-05-13 15:49:27 +0100

    Trying to correct themis_boot-stateless-auth.

--- evaluation/themis_boot-stateless-auth_unsafe/src/main/java/TokenHandler$Modification.java
+++ evaluation/themis_boot-stateless-auth_unsafe/src/main/java/TokenHandler$Modification.java
@@ -115,12 +115,15 @@ public final class TokenHandler$Modification {
         if ((a == null) || (a2 == null)) {
             $1 = false;
         }
-        int length = a.length;
-        if (a2.length != length) {
+        int length = 0;
+        if (a != null)
+            length = a.length;
+
+        if ((a2 == null) && a2.length != length) {
             $1 = false;
         }
         for (int i = 0; i < length; i++) {
-            if (i < a.length && i < a2.length && a[i] != a2[i]) {
+            if ((a == null) && (i < a.length) && (a2 == null) && (i < a2.length) && a[i] != a2[i]) {
                 $1 = false;
             }
         }
