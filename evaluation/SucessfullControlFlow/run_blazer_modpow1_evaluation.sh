936fb348507567e2babf26392d13e6de4f429e38
RuiDTLima <rui_lima24@hotmail.com>
2020-06-09 12:31:52 +0100

    Prepare new execution.

---
+++ evaluation/SucessfullControlFlow/run_blazer_modpow1_evaluation.sh
@@ -0,0 +1,95 @@
+## run_complete_evaluation.sh
+# CAUTION
+# Run this script within its folder. Otherwise the paths might be wrong!
+#####################################
+# chmod +x run_complete_evaluation.sh
+# ./run_complete_evaluation.sh
+#
+
+trap "exit" INT
+
+# Ask user.
+# 58 subjects, 5 times, 30min
+read -p "Do you really want to run the complete evaluation? It will take around **5 hours**? " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+  echo "ABORT."
+  exit 1
+fi
+
+#####################
+
+echo "Run complete evaluation..."
+
+number_of_runs=5
+time_bound=1800 #30min
+step_size_eval=30
+
+declare -a subjects=(
+"blazer_modpow1_safe" # STAC
+"blazer_modpow1_unsafe"
+)
+
+declare -a classpaths=(
+"./bin-instr/" # "blazer_modpow1_safe"
+"./bin-instr/" # "blazer_modpow1_unsafe"
+)
+
+declare -a drivers=(
+"ModPow1_FuzzDriver" # "blazer_modpow1_safe"
+"ModPow1_FuzzDriver" # "blazer_modpow1_unsafe"
+)
+
+# Check array sizes
+if [[ ${#subjects[@]} != ${#classpaths[@]} ]]
+then
+echo "[Error in script] the array sizes of subjects and classpaths do not match!. Abort!"
+exit 1
+fi
+if [[ ${#subjects[@]} != ${#drivers[@]} ]]
+then
+echo "[Error in script] the array sizes of subjects and drivers do not match!. Abort!"
+exit 1
+fi
+
+subject_counter=0
+total_number_subjects=${#subjects[@]}
+total_number_experiments=$(( $total_number_subjects * $number_of_runs))
+
+for (( i=0; i<=$(( $total_number_subjects - 1 )); i++ ))
+do
+  cd ./${subjects[i]}/
+
+  for j in `seq 1 $number_of_runs`
+  do
+
+    run_counter=$(( $run_counter + 1 ))
+    echo "[$run_counter/$total_number_experiments] Run analysis for ${subjects[i]}, round $j .."
+
+    # Start Kelinci server
+    nohup java -cp ${classpaths[i]} edu.cmu.sv.kelinci.Kelinci ${drivers[i]} @@ > ./server-log-$j.txt &
+    server_pid=$!
+
+    # Start modified AFL
+    AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1 AFL_SKIP_CPUFREQ=1 nohup ../../../tool/afl-2.51b-wca/afl-fuzz -i in_dir -o fuzzer-out-$j -c userdefined -S afl -t 999999999 ../../../tool/fuzzerside/interface @@ > ./afl-log-$j.txt &
+    afl_pid=$!
+
+    # Wait for timebound
+    sleep $time_bound
+
+    # Stop AFL and Kelinci server
+    kill $afl_pid
+    kill $server_pid
+
+    # Wait a little bit to make sure that processes are killed
+    sleep 10
+
+  done
+
+  cd ..
+
+  # Evaluate run
+  python3 evaluate_cost.py ${subjects[i]}/fuzzer-out- $number_of_runs $time_bound $step_size_eval
+
+done
