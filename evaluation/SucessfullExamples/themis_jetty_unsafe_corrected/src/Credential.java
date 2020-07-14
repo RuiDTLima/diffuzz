a8e621ee434d34d0a06c71c507888d9233787994
RuiDTLima <rui_lima24@hotmail.com>
2020-07-14 11:37:14 +0100

    Correct test templates.

--- evaluation/SucessfullExamples/themis_jetty_unsafe_corrected/src/Credential.java
+++ evaluation/SucessfullExamples/themis_jetty_unsafe_corrected/src/Credential.java
@@ -15,25 +15,36 @@ public class Credential {
     }
 
     public static boolean stringEquals_original(String s1, String s2) {
+        boolean $2 = false;
         boolean $1 = false;
         if (s1 == s2) {
             $1 = true;
+        } else {
+            $2 = true;
         }
         int n = s1.length();
+        int $5 = n;
         if (n == s2.length()) {
             char[] v1 = s1.toCharArray();
             char[] v2 = s2.toCharArray();
             int i = 0;
             $1 = true;
             while ((n--) != 0) {
-                if ((i < v1.length) && (i < v2.length) && v1[i] != v2[i]) {
+                if (((i < v1.length) && (i < v2.length)) && (v1[i] != v2[i])) {
                     $1 = false;
+                } else {
+                    $2 = false;
                 }
                 i++;
             } 
-            return $1;
+        } else {
+            char[] $3 = s1.toCharArray();
+            int $4 = 0;
+            $2 = true;
+            while (($5--) != 0) {
+                $4++;
+            } 
         }
         return $1;
     }
-
 }
--- evaluation/SucessfullExamples/themis_oacc_unsafe_corrected/src/com/acciente/oacc/PasswordCredentials.java
+++ evaluation/SucessfullExamples/themis_oacc_unsafe_corrected/src/com/acciente/oacc/PasswordCredentials.java
@@ -52,13 +52,30 @@ public abstract class PasswordCredentials implements Credentials {
         }
 
         @Override
-        public boolean equals(Object other) {
+        /*public boolean equals(Object other) {
             boolean $1;
             if (PasswordCredentials.Impl.this == other) {
                 $1 = true;
             }
             if ((other == null) || (getClass() != other.getClass())) {
                 $1 = false;
+            }
+            PasswordCredentials.Impl impl = ((PasswordCredentials.Impl) (other));
+            $1 = ArraysIsEquals(password, impl.password);
+            return $1;
+        }*/
+        public boolean equals(Object other) {
+            boolean $2;
+            boolean $1;
+            if (PasswordCredentials.Impl.this == other) {
+                $1 = true;
+            } else {
+                $2 = true;
+            }
+            if ((other == null) || (getClass() != other.getClass())) {
+                $1 = false;
+            } else {
+                $2 = false;
             }
             PasswordCredentials.Impl impl = ((PasswordCredentials.Impl) (other));
             $1 = ArraysIsEquals(password, impl.password);
